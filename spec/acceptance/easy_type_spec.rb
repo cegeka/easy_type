e45d2ee174d3c1ae1610dc3a6e4885d16b4b1e46
Paul Herbosch <paul.herbosch@cegeka.be>
2016-06-30 10:56:59 +0200

    [EASY_TYPE] trying to access one method in this module

--- spec/acceptance/easy_type_spec.rb
+++ spec/acceptance/easy_type_spec.rb
@@ -6,31 +6,12 @@ describe 'easy_type' do
     it 'should work with no errors' do
       pp = <<-EOS
 
-        require 'spec_helper'
         require 'easy_type/file_includer'
-        
-        describe EasyType::FileIncluder do
-          include EasyType::FileIncluder
-        
-          context "a file does exist" do
-        
-            before do
-              include_file "easy_type/include_check.rb"
-            end
-        
-            it "evaluates the ruby code in the file" do
-              expect(file_is_included).to be true
-            end
-          end
-        
-          context "a file does not exist" do
-        
-            it "raises an ArgumentError" do
-              expect{ include_file "easy_type/nonexisting_file.rb"}.to raise_error(ArgumentError)
-            end
-          end
-        
-        end
+        require 'easy_type/mungers'
+
+        $test = unsafe_munge('100M')
+        puts "TEST input was 100M, test output is $test"
+ 
       EOS
 
       # Run it twice and test for idempotency
